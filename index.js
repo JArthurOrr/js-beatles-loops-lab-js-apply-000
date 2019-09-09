823afa977aa6b5edf72266f796bf7776aedf2edc
John Orr <john_a_orr@yahoo.com>
2019-09-09 02:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(musicians ){
+	var Array= ["John Lennon plays Guitar",
+	"Paul McCartney plays bass Guitar",
+	"George Harrison plays Lead Guitar",
+	"Ringo Starr plays Drums"
+	];
+	for(var i= 0; i < musicians.length; i += 1 ){
+	Array.push(musicians[i] + "plays" + instruments[i])
+}
+return Array;
+}
